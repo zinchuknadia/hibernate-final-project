a8e6b8ad1aa4a1c90f6accb3131cf927d264bb02
zinchuknadia <nadiia.zinchuk.oi.2023@lpnu.ua>
2025-12-28 02:49:05 +0200

    Add method to transform data for redis caching

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -3,6 +3,7 @@ package com.javarush;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javarush.dao.*;
 import com.javarush.domain.*;
+import com.javarush.redis.*;
 import io.lettuce.core.RedisClient;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -12,6 +13,8 @@ import org.hibernate.cfg.Environment;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static java.util.Objects.nonNull;
 
@@ -61,6 +64,7 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         List<City> allCities = main.fetchData(main);
+        List<CityCountry> preparedData = main.transformData(allCities);
         main.shutdown();
     }
 
@@ -79,6 +83,36 @@ public class Main {
             session.getTransaction().commit();
             return allCities;
         }
+    }
+
+    private List<CityCountry> transformData(List<City> cities) {
+        return cities.stream().map(city -> {
+            CityCountry res = new CityCountry();
+            res.setId(city.getId());
+            res.setName(city.getName());
+            res.setPopulation(city.getPopulation());
+            res.setDistrict(city.getDistrict());
+
+            Country country = city.getCountry();
+            res.setAlternativeCountryCode(country.getAlternativeCode());
+            res.setContinent(country.getContinent());
+            res.setCountryCode(country.getCode());
+            res.setCountryName(country.getName());
+            res.setCountryPopulation(country.getPopulation());
+            res.setCountryRegion(country.getRegion());
+            res.setCountrySurfaceArea(country.getSurfaceArea());
+            Set<CountryLanguage> countryLanguages = country.getLanguages();
+            Set<Language> languages = countryLanguages.stream().map(cl -> {
+                Language language = new Language();
+                language.setLanguage(cl.getLanguage());
+                language.setOfficial(cl.getOfficial());
+                language.setPercentage(cl.getPercentage());
+                return language;
+            }).collect(Collectors.toSet());
+            res.setLanguages(languages);
+
+            return res;
+        }).collect(Collectors.toList());
     }
 
     private void shutdown() {
